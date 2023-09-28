c6b3dfc0d3172395143b4fea621377f6a446bd52
Jerry Kurtin <gerald.kurtin7@gmail.com>
2023-09-28 13:12:09 +0800

    initial setup completed

---
+++ EntryNode/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"encoding/json"
+	"github.com/gorilla/mux"
+)
+
+// Note: values MUST start with capital letter to be exported
+type SampleStruct struct {
+	Val string `json:"val"`
+}
+
+func healthCheck(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Health check called")
+
+	sampleStruct := SampleStruct {
+		Val: "success!",
+	}
+
+	response, err := json.Marshal(sampleStruct)
+	if err != nil {
+		fmt.Println("error marshalling data")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
+
+func main() {
+	port := 3000
+
+	// create a new router
+	router := mux.NewRouter().StrictSlash(true)
+
+	// expose endpoints
+	router.HandleFunc("/health", healthCheck).Methods("GET")
+
+	// Catch all undefined endpoints
+	router.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Route undefined"))
+	})
+
+	// start service
+	fmt.Printf("Listening on port %d\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), router))
+}
