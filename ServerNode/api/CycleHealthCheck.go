a801e5c4cbe1a0ac0e82aed9695610068710cb91
Jerry Kurtin <gerald.kurtin7@gmail.com>
2023-11-22 14:03:18 +0800

    minor changes, now we return number of nodes

--- ServerNode/api/CycleHealthCheck.go
+++ ServerNode/api/CycleHealthCheck.go
@@ -4,16 +4,21 @@ import (
 	"ServerNode/constants"
 	"ServerNode/util"
 	"fmt"
+	"encoding/json"
 	"net/http"
-
 	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
+type CycleHealthResponse struct {
+	CycleSize int
+}
+
 func (h *Handler) CycleHealthCheck(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("[Debug] CycleHealthCheck called\n")
-	// Parse variables from url
+
+	// Parse variables from url -------------------------------------------
 	StartingNodeHash := mux.Vars(r)["StartingNodeHash"]
 	if StartingNodeHash == "nil" {
 		StartingNodeHash = h.NodeInfo.NodeHash
@@ -28,16 +33,16 @@ func (h *Handler) CycleHealthCheck(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("[Debug] given StartingNodeHash: %s, FinishedLoop: %t\n", StartingNodeHash, FinishedLoop)
 	fmt.Printf("[Debug] current node hash: %s\n", h.NodeInfo.NodeHash)
 
-	// Case: we reach an equal or greater node from the start after having looped
+	// We've cycled back, return -------------------------------------------
 	if FinishedLoop && h.NodeInfo.NodeHash >= StartingNodeHash {
-		fmt.Printf("[Debug] Finished the loop\n")
-		w.WriteHeader(http.StatusOK)
+		fmt.Printf("[Debug] cycle complete, returning!\n")
+		WriteResponse(w, CycleHealthResponse{CycleSize: 1}, http.StatusOK)
 		return
 
 	} else {
 		fmt.Printf("[Debug] continuing on the loop\n")
 		for i := 0; i < min(h.NodeInfo.StoredNbrs, len(h.NodeInfo.SuccessorArray)); i++ {
-			fmt.Printf("[Debug] sending msg to %s\n",h.NodeInfo.SuccessorArray[i])
+			fmt.Printf("[Debug] sending msg to %s\n", h.NodeInfo.SuccessorArray[i])
 			// Case: we've looped
 			if util.Sha256String(h.NodeInfo.SuccessorArray[i]) <= StartingNodeHash {
 				fmt.Printf("[Debug] setting finishedLoop to true, next node hash of %s <= starting hash of %s\n", util.Sha256String(h.NodeInfo.SuccessorArray[i]), StartingNodeHash)
@@ -47,18 +52,24 @@ func (h *Handler) CycleHealthCheck(w http.ResponseWriter, r *http.Request) {
 			// Case: check the next descendant with timeout
 			requestEndpoint := fmt.Sprintf("/api/cycleHealth/%s/%t", StartingNodeHash, FinishedLoop)
 			resp, err := h.Requester.SendRequest(h.NodeInfo.SuccessorArray[i], requestEndpoint, http.MethodGet, constants.REQUEST_TIMEOUT)
-			
+
 			if err != nil {
 				fmt.Printf("[Debug] child %s is not healthy, trying next\n", h.NodeInfo.SuccessorArray[i])
-			
+
 			} else if resp.StatusCode != http.StatusOK {
 				fmt.Printf("[Debug] next node is reporting break in cycle with status code: %d\n", resp.StatusCode)
 				w.WriteHeader(resp.StatusCode)
 				return
-			
+
 			} else {
-				fmt.Printf("[Debug] received healthCheck response from child!\n")
-				w.WriteHeader(resp.StatusCode)
+				healthResp, err := readResponseBody(resp)
+				if (err != nil) {
+					fmt.Printf("[Error] failed to decode response body: %s\n", err.Error())
+				}
+
+				healthResp.CycleSize++
+				fmt.Printf("[Debug] received healthCheck response from child! nodes: %d\n", healthResp.CycleSize)
+				WriteResponse(w, healthResp, http.StatusOK)
 				return
 			}
 		}
@@ -66,4 +77,30 @@ func (h *Handler) CycleHealthCheck(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Printf("[Error] all descendants have failed for current NodeUrl |%s|\n", h.NodeInfo.NodeUrl)
 	w.WriteHeader(http.StatusInternalServerError)
+}
+
+func readResponseBody(resp *http.Response) (*CycleHealthResponse, error) {
+	defer resp.Body.Close()
+	var cycleHealthResponse CycleHealthResponse
+
+	err := json.NewDecoder(resp.Body).Decode(&cycleHealthResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return &cycleHealthResponse, nil
+}
+
+func WriteResponse(w http.ResponseWriter, response interface{}, httpCode int) (error) {
+	marshalledResp, err := json.Marshal(response)
+	if err != nil {
+		fmt.Println("error marshalling data")
+		w.WriteHeader(http.StatusInternalServerError)
+		return err
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(httpCode)
+	w.Write(marshalledResp)
+	return nil
 }
--- ServerNode/main.go
+++ ServerNode/main.go
@@ -32,9 +32,9 @@ func main() {
 	// create a handler that stores and updates our node information
 	handler := api.NewHandler(fmt.Sprintf("http://10.12.103.97:%d", port), STORED_NBRS)
 	// for testing purposes, you can run nodes on localhost 2000, 3000, and 4000. Then, you can remove node 3000 and it will still be successful
-	// handler.NodeInfo.SuccessorArray = append(handler.NodeInfo.SuccessorArray, fmt.Sprintf("http://10.12.103.97:%d", (port+1000) % 5000))
-	// handler.NodeInfo.SuccessorArray = append(handler.NodeInfo.SuccessorArray, fmt.Sprintf("http://10.12.103.97:%d", (port+2000) % 5000))
-	// handler.NodeInfo.SuccessorArray = append(handler.NodeInfo.SuccessorArray, fmt.Sprintf("http://10.12.103.97:%d", (port+3000) % 5000))
+	handler.NodeInfo.SuccessorArray = append(handler.NodeInfo.SuccessorArray, fmt.Sprintf("http://10.12.103.97:%d", (port+1000) % 5000))
+	handler.NodeInfo.SuccessorArray = append(handler.NodeInfo.SuccessorArray, fmt.Sprintf("http://10.12.103.97:%d", (port+2000) % 5000))
+	handler.NodeInfo.SuccessorArray = append(handler.NodeInfo.SuccessorArray, fmt.Sprintf("http://10.12.103.97:%d", (port+3000) % 5000))
 
 
 	fmt.Printf("[Debug] set up node %s\n", handler.NodeInfo)
