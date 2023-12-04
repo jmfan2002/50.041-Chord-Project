d551501e5c03cad0017521b5c372498512e832fa
James Fan <jmfan2002@gmail.com>
2023-12-04 17:37:17 +0800

    fun names

--- addNode.sh
+++ addNode.sh
@@ -1,19 +1,28 @@
 if [ -z "$1" ]; then
+    echo "ERROR: Please pass server name, should be unique"
+    exit
+fi
+if [ -z "$2" ]; then
     echo "ERROR: Please pass server number, should be higher than current server count"
     exit
 fi
+if [ -z "$3" ]; then
+    echo "ERROR: Please pass tolerance number"
+    exit
+fi
 
-SERVER_ID=$1
-TOLERANCE=1
+SERVER_NAME=$1
+SERVER_ID=$2
+TOLERANCE=$3
 
 DOCKER_TEMPLATE='
 version: "3.4"
 
 services:
-  server_nodeSERVER_ID:
+  SERVER_NAME:
     build: ./ServerNode
     image: server_node
-    entrypoint: ["./ServerNode", "PORT_NUM", "TOLERANCE", "server_nodeSERVER_ID", "entry_node:3000"]
+    entrypoint: ["./ServerNode", "PORT_NUM", "TOLERANCE", "SERVER_NAME", "entry_node:3000"]
     ports:
       - "PORT_NUM:PORT_NUM"
     deploy:
@@ -25,7 +34,7 @@ services:
 networks:
   chord-network:
 '
-COMPOSE_STR="${DOCKER_TEMPLATE//SERVER_ID/${SERVER_ID}}"
+COMPOSE_STR="${DOCKER_TEMPLATE//SERVER_NAME/${SERVER_NAME}}"
 COMPOSE_STR="${COMPOSE_STR//TOLERANCE/${TOLERANCE}}"
 COMPOSE_STR="${COMPOSE_STR//PORT_NUM/$((SERVER_ID + 4000))}"
 echo "${COMPOSE_STR}">"./docker-compose.yml"
